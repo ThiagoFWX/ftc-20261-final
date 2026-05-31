f105867bfdb7c78ecdf069e631a93fa9fa3d2ef6
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 18:15:17 -0300

    feat: adicionado um constructor na classe TransicaoPilha

--- Parte2/models/TransicaoPilha.cs
+++ Parte2/models/TransicaoPilha.cs
@@ -1,8 +1,17 @@
 ﻿public class TransicaoPilha
 {
     public string estadoAtual { get; set; }
-    public string simboloEntrada { get; set; }
-    public string topoPilha { get; set; }
+    public char simboloEntrada { get; set; }
+    public char topoPilha { get; set; }
     public string proximoEstado { get; set; }
     public string acaoPilha { get; set; }
+
+    public TransicaoPilha(string estadoAtual, char simboloEntrada, char topoPilha, string proximoEstado, string acaoPilha)
+    {
+        this.estadoAtual = estadoAtual;
+        this.simboloEntrada = simboloEntrada;
+        this.topoPilha = topoPilha;
+        this.proximoEstado = proximoEstado;
+        this.acaoPilha = acaoPilha;
+    }
 }
