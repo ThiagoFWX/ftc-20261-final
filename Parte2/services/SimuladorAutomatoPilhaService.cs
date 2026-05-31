c65a47ef60dc01a2e8c0692f9c5ab9f227c79e9d
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 15:22:53 -0300

    feat: implementado o método Simular com inicialização de estado e pilha

--- Parte2/services/SimuladorAutomatoPilhaService.cs
+++ Parte2/services/SimuladorAutomatoPilhaService.cs
@@ -6,4 +6,25 @@
     {
         this.automato = automato;
     }
+
+    public Stack<string> Simular(string entrada)
+    {
+        string estadoAtual = automato.estadoInicial;
+
+        var pilha = new Stack<string>();
+
+        pilha.Push("Z");
+
+        Console.WriteLine($"Estado inicial: {estadoAtual}");
+        Console.WriteLine($"Entrada: {entrada}");
+
+        for (int i = 0; i < entrada.Length; i++)
+        {
+            char simbolo = entrada[i];
+
+            Console.WriteLine($"Lendo o símbolo: {simbolo}");
+        }
+
+        return pilha;
+    }
 }
