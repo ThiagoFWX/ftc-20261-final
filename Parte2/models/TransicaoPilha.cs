20a0aefec6c11ef7a9aaf4074c0253d5524b72e9
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 12:35:31 -0300

    feat: adicionada a estrutura de dados TransicaoPilha

--- Parte2/models/TransicaoPilha.cs
+++ Parte2/models/TransicaoPilha.cs
@@ -1,1 +1,8 @@
-﻿
+﻿public class TransicaoPilha
+{
+    public string EstadoAtual { get; set; }
+    public string SimboloEntrada { get; set; }
+    public string TopoPilha { get; set; }
+    public string ProximoEstado { get; set; }
+    public string AcaoPilha { get; set; }
+}
