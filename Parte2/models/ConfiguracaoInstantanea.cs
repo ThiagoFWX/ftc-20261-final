25a29a40acba06e192e5a0a6288a6a35dc38e195
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 19:37:47 -0300

    feat: classe ConfiguracaoInstantanea criada

--- Parte2/models/ConfiguracaoInstantanea.cs
+++ Parte2/models/ConfiguracaoInstantanea.cs
@@ -1,1 +1,21 @@
-﻿
+﻿public class ConfiguracaoInstantanea
+{
+    public string estado { get; set; }
+    public string entradaRestante { get; set; }
+    public string conteudoPilha { get; set; }
+
+    public ConfiguracaoInstantanea(string estado, string entradaRestante, Stack<char> pilha)
+    {
+        this.estado = estado;
+        this.entradaRestante = entradaRestante;
+
+        char[] pilhaArr = pilha.ToArray();
+        Array.Reverse(pilhaArr);
+        this.conteudoPilha = new string(pilhaArr);
+    }
+
+    public override string ToString()
+    {
+        return $"Estado: {this.estado} | Entrada: '{this.entradaRestante}' | Pilha: [{this.conteudoPilha}]";
+    }
+}
