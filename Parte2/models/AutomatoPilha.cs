16def6a7955d5d2e89c964204641e3b5d599a19f
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 12:54:12 -0300

    feat: adicionada a classe AutomatoPilha com sua estrutura básica

--- Parte2/models/AutomatoPilha.cs
+++ Parte2/models/AutomatoPilha.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Collections.Generic;
+
+public class AutomatoPilha
+{
+    public string EstadoInicial { get; set; }
+
+    public List<string> EstadosFinais { get; set; }
+
+    public List<TransicaoPilha> Transicoes { get; set; }
+}
