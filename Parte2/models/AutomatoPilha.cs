65b2bbc34740049ee57520b6ea9f9d0217e353e3
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 18:20:11 -0300

    feat: adicionado um constructor na classe AutomatoPilha

--- Parte2/models/AutomatoPilha.cs
+++ Parte2/models/AutomatoPilha.cs
@@ -2,9 +2,20 @@
 
 public class AutomatoPilha
 {
+    public List<string> estados { get; set; }
+    public List<char> alfabetoEntrada { get; set; }
+    public List<char> alfabetoPilha { get; set; }
+    public Dictionary<(string, char, char), List<TransicaoPilha>> transicoes { get; set; }
     public string estadoInicial { get; set; }
+    public char simboloInicial { get; set; }
+    public List<string> estadosAceitacao { get; set; }
 
-    public List<string> estadosFinais { get; set; }
-
-    public List<TransicaoPilha> transicoes { get; set; }
+    public AutomatoPilha()
+    {
+        this.estados = new List<string>();
+        this.alfabetoEntrada = new List<char>();
+        this.alfabetoPilha = new List<char>();
+        this.transicoes = new Dictionary<(string, char, char), List<TransicaoPilha>>();
+        this.estadosAceitacao = new List<string>();
+    }
 }
