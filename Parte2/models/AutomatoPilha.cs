68fca340c2bcc9db1bb31e03d9663ff411e1ebed
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-05-31 22:36:48 -0300

    feat: função para obter transições criada

--- Parte2/models/AutomatoPilha.cs
+++ Parte2/models/AutomatoPilha.cs
@@ -7,15 +7,39 @@ public class AutomatoPilha
     public List<char> alfabetoPilha { get; set; }
     public Dictionary<(string, char, char), List<TransicaoPilha>> transicoes { get; set; }
     public string estadoInicial { get; set; }
-    public char simboloInicial { get; set; }
-    public List<string> estadosAceitacao { get; set; }
+    public char simboloInicialPilha { get; set; }
 
-    public AutomatoPilha()
+    public AutomatoPilha(List<string> estados,
+        List<char> alfabetoEntrada,
+        List<char> alfabetoPilha,
+        Dictionary<(string, char, char), List<TransicaoPilha>> transicoes,
+        string estadoInicial,
+        char simboloInicialPilha)
     {
-        this.estados = new List<string>();
-        this.alfabetoEntrada = new List<char>();
-        this.alfabetoPilha = new List<char>();
-        this.transicoes = new Dictionary<(string, char, char), List<TransicaoPilha>>();
-        this.estadosAceitacao = new List<string>();
+        this.estados = estados;
+        this.alfabetoEntrada = alfabetoEntrada;
+        this.alfabetoPilha = alfabetoPilha;
+        this.transicoes = transicoes;
+        this.estadoInicial = estadoInicial;
+        this.simboloInicialPilha = simboloInicialPilha;
+    }
+
+    public List<TransicaoPilha> ObterTransicoes(string estado, char simboloEntrada, char topoPilha)
+    {
+        var chave = (estado, simboloEntrada, topoPilha);
+
+        if (this.transicoes.ContainsKey(chave))
+        {
+            possiveisTransicoes.AddRange(this.transicoes[chave]);
+        }
+
+        var chaveLambda = (estado, '\0', topoPilha);
+
+        if (this.transicoes.ContainsKey(chaveLambda))
+        {
+            possiveisTransicoes.AddRange(this.transicoes[chaveLambda]);
+        }
+
+        return possiveisTransicoes;
     }
 }
