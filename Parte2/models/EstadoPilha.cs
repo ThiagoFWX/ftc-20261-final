c910ac4bd40ddde4fdb3959c96c46389b2064574
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2026-06-04 11:02:39 -0300

    feat: criada a classe EstadoPilha com estados do processamento

--- Parte2/models/EstadoPilha.cs
+++ Parte2/models/EstadoPilha.cs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace Parte2.Models;
+
+public static class EstadoPilha
+{
+    public const string LendoAs = "lendo_as";
+    public const string LendoBs = "lendo_bs";
+
+    public const string EmpilhandoPalindromo = "empilhando_palindromo";
+    public const string DesempilhandoPalindromo = "desempilhando_palindromo";
+}
