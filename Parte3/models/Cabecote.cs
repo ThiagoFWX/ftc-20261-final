e8ab001d40ea285a91fa5a67dc311d3975edac68
Thiago <thiagofwxavier@gmail.com>
2026-06-05 11:48:40 -0300

    Implementação das classes Fita e Cabecote.

--- Parte3/models/Cabecote.cs
+++ Parte3/models/Cabecote.cs
@@ -6,6 +6,37 @@ namespace Parte3.models
 {
     public class Cabecote
     {
-        public int Posicao { get; set; }
+        public int Posicao
+        {
+            get;
+            set;
+        }
+
+        public Cabecote()
+        {
+            Posicao = 0;
+        }
+
+        public void Mover(
+            char direcao)
+        {
+            if (
+                direcao == 'R')
+            {
+                Posicao++;
+            }
+
+            else if (
+                direcao == 'L')
+            {
+                Posicao--;
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                Posicao.ToString();
+        }
     }
 }
--- Parte3/models/Fita.cs
+++ Parte3/models/Fita.cs
@@ -6,7 +6,43 @@ namespace Parte3.models
 {
     public class Fita
     {
-        public Dictionary<int, string> Celdas { get; set; }
-
+        public Dictionary<int, string> Celulas { get; set; }
+        public char Branco { get; set; }
+        public Fita(string entrada,  char branco)
+        {
+            Branco = branco;
+            Celulas = new Dictionary<int, string>();
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                Celulas[i] = entrada[i].ToString();
+            }
+        }
+        public char Ler(int posicao)
+        {
+            if (Celulas.ContainsKey(posicao))
+            {
+                return Celulas[posicao][0];
+            }
+            return Branco;
+        }
+        public void Escrever(int posicao, char simbolo)
+        {
+            Celulas[posicao] = simbolo.ToString();
+        }
+        public override string ToString()
+        {
+            if(Celulas.Count == 0)
+            {
+                return Branco.ToString();
+            }
+            int menor = Celulas.Keys.Min();
+            int maior = Celulas.Keys.Max();
+            string resultado = "";
+            for (int i = menor; i <= maior; i++)
+            {
+                resultado += Ler(i);
+            }
+            return resultado;
+        }
     }
 }
